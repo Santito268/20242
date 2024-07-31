872366133c2ab0c7cd6ca087af8970eb2aa07632
Santito268 <santiagocarrillopineros@gmail.com>
2024-07-31 16:57:50 -0500

    Update Producto.java

--- src/clases/basico/Producto.java
+++ src/clases/basico/Producto.java
@@ -4,9 +4,6 @@ public class Producto {
 
     //Consultando o Actualizando(Insert, eliminar, actualizar)
     public String consultarProductos(){
-        if("productos" == null){
-            //Cerrar Sesion
-        }
         return "productos";
     }
 }
